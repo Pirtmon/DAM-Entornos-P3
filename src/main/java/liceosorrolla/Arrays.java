60ec58c0a5af35d3485491f79bb0f91c8a4de8e9
DAM_1 <DAM_1@PB04>
2020-01-24 21:10:30 +0100

    Mejora 03

--- src/main/java/liceosorrolla/Arrays.java
+++ src/main/java/liceosorrolla/Arrays.java
@@ -1,11 +1,38 @@
+  
 package main.java.liceosorrolla;
 
-
-
 public class Arrays {
-
+	
+	
+	
 	public static void main(String[] args) {
-		System.out.println("");
-	}
+		
+		int[] array = {666,66,6,9,99,999};
+		
+		System.out.println(mediana(array));
+		
+	}//MAIN
+	
+	public static int mediana(int []array) {
+		
+		int mediana = array[0];
+		int numero;
+		
+		numero = array.length/2;
+		
+		if (array.length % 2 == 0) {
+			
+			mediana = (array[numero] + array[numero - 1]) /2;
+			
+		}	
+		
+		else { 
+			
+			mediana = array[numero];
+			
+		}
+		
+		return mediana;
+	}//mediana
 
-}
+}//CLASS
