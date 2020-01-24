5522904fd9ac3dfb7944a5f29b297a4fc9454e10
DAM_1 <DAM_1@PB04>
2020-01-24 20:02:20 +0100

    Oke2

--- src/main/java/liceosorrolla/Numero.java
+++ src/main/java/liceosorrolla/Numero.java
@@ -5,7 +5,7 @@ package main.java.liceosorrolla;
 public class Numero {
 
 	public static void main(String[] args) {
-		System.out.println("Arrays,Numero,Cadena");
+		System.out.println("");
 	}
 
 }
