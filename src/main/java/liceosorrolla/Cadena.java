99e5bcdd0bf492bb6d376ee506fb3aee00d51dfc
yane- <yane-@DESKTOP-HOOQ99R.home>
2020-01-31 23:05:11 +0100

    Mejora 06

--- src/main/java/liceosorrolla/Cadena.java
+++ src/main/java/liceosorrolla/Cadena.java
@@ -1,12 +1,15 @@
 package main.java.liceosorrolla;
 
-
-
 public class Cadena {
-
+	
+	public static void palabra(String texto) {
+		
+		System.out.println("La longitud de la palabra es: "+texto.length()+" caracteres");
+	}
+	
 	public static void main(String[] args) {
 		String texto = "cadena";
-		
+		palabra(texto);
 	}
 
 }
