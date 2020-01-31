20ed958c5941aa7a3b943a345b227a23c28350a7
DAM_1 <DAM_1@PB05>
2020-01-31 20:29:32 +0100

    Mejora 02

--- src/main/java/liceosorrolla/Arrays.java
+++ src/main/java/liceosorrolla/Arrays.java
@@ -3,9 +3,21 @@ package main.java.liceosorrolla;
 
 
 public class Arrays {
+	
+	public static int minimo(int []array) {
+		int menor = array[0];
+		for(int i = 0; i < array.length; i++){
+			if(array[i] < menor) 
+			{
+				menor = array[i];
+			}
+		}
+		return menor;
+	}
 
 	public static void main(String[] args) {
 		int[] array = {1,2,3,4,5};
+		System.out.println(minimo(array));
 	}
 
 }
