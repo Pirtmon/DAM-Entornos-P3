0463f822aa778d24eab759f8ad65e2ab3b9393c6
yanella98 <yane-@DESKTOP-HOOQ99R.home>
2020-01-31 23:13:12 +0100

    Mejora 07

--- src/main/java/liceosorrolla/Cadena.java
+++ src/main/java/liceosorrolla/Cadena.java
@@ -7,9 +7,15 @@ public class Cadena {
 		System.out.println("La longitud de la palabra es: "+texto.length()+" caracteres");
 	}
 	
+	public static void cambio(String texto) {
+		System.out.println("La palabra en mayusculas es: "+texto.toUpperCase());
+		System.out.println("La palabra en minusculas es: "+texto.toLowerCase());
+	}
+	
 	public static void main(String[] args) {
 		String texto = "cadena";
 		palabra(texto);
+		cambio(texto);
 	}
 
 }
