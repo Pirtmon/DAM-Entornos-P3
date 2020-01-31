e28f417c4061be37864e2b54497472584b527c48
DAM_1 <DAM_1@PB03>
2020-01-31 21:10:46 +0100

    Mejora 1

--- src/main/java/liceosorrolla/Arrays.java
+++ src/main/java/liceosorrolla/Arrays.java
@@ -1,8 +1,22 @@
 package main.java.liceosorrolla;
 
-
-
 public class Arrays {
+	
+	public static void maximo(int []array) {
+
+		int max = 0;
+
+		for(int i = 0; i < array.length; i++)
+		{
+			
+			if(max<array[i])
+			{
+				max=array[i];
+			}
+		}
+ 
+		System.out.println("El valor máximo es: " + max);
+	}
 	
 	public static int minimo(int []array) {
 		int menor = array[0];
@@ -18,6 +32,7 @@ public class Arrays {
 	public static void main(String[] args) {
 		int[] array = {1,2,3,4,5};
 		System.out.println(minimo(array));
+		maximo(array);
 	}
 
 }
